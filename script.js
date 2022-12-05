681cb7189d416e87ac8450acf59164fb96e45e80
Aghil P Wilson <aghilpwilson1994@gmail.com>
2022-12-05 09:41:46 +0530

    code refactored

--- script.js
+++ script.js
@@ -1,98 +1,84 @@
+window.addEventListener("load", (event) => {
+    showData()
+  });
 
-showData()
-const addBtn = document.querySelector(".add-btn");
+
+//-----------------------//popup references--------------------
 const popup = document.querySelector(".popup-wrapper")
 const close = document.querySelector(".popup-close")
+let saveBtn = document.querySelector(".save-btn")
+const addBtn = document.querySelector(".add-btn");
 const submitBtn = document.querySelector("#submit-btn")
+
+
+//-------------------Button References------------------------------
 const deleteBtn = document.querySelector("#delete-btn");
 const editBtn = document.querySelector("#edit-btn");
-let saveBtn = document.querySelector(".save-btn")
-let saveIdx = document.getElementById("saveindex")
 let clearAllBtn = document.querySelector(".clear-all")
+
+//------------------------Index catcher Ref-------------------------
+let saveIdx = document.getElementById("saveindex")
+
+//-----------------Main Fn Ref ------------------
 let searchBox = document.querySelector("#search-bar")
 let mainDataBox = document.querySelector("#main-data")
 let sortEl = document.querySelector("#sort")
-//data reference
 
+
+//---------------------Reference of inputFields---------------------
 let name = document.querySelector("#name")
 let panNum = document.querySelector("#pan")
 let age = document.querySelector("#age")
 let qualification = document.querySelector("#qualification")
 
-function reset(){
+//--------------------------------------------------------------------------------------------
+
+function reset() {
     name.value = ""
     panNum.value = ""
     age.value = ""
     qualification.value = ""
 }
 
+
+
+function checkAndGetDataPersistAlready() {
+    let dataPersist = localStorage.getItem("localData")
+
+    if (dataPersist == null) {
+        dataArr = []
+        return dataArr
+    } else {
+        dataArr = JSON.parse(dataPersist);
+        return dataArr
+    }
+}
+
+
 function showModal() {
     popup.style.display = "block";
 }
+
+
+
 function closeModal() {
     popup.style.display = "none"
 }
+
+
 addBtn.addEventListener("click", showModal)
 
 close.addEventListener("click", closeModal)
 
-
-submitBtn.addEventListener("click", function (e) {
-    e.preventDefault()
-    nameVal = name.value;
-    panNumVal = panNum.value;
-    ageVal = age.value;
-    qualificationVal = qualification.value;
-
-    if (nameVal.trim() != 0 && panNumVal.trim() != 0 && ageVal.trim() != 0 && qualificationVal.trim() != 0) {
-        //checking weather the data is present i local storage already
-        let dataPersist = localStorage.getItem("localtask")
-
-        if (dataPersist == null) {
-            dataArr = []
-        } else {
-            dataArr = JSON.parse(dataPersist);
-        }
-        let obj = {
-            "personName": nameVal,
-            "personPan": panNumVal,
-            "personAge": ageVal,
-            "personQualification": qualificationVal
-        }
-        dataArr.push(obj)
-        localStorage.setItem("localtask", JSON.stringify(dataArr))
-    }
-
-    if(name.value == ""||panNum.value == ""||age.value == ""||qualification.value==""){
-        alert("input fields cannot be empty");
-    }else{
-        closeModal()
-    }
-
-    showData()
-    reset()
-    
-})
-
-function showData() {
-    // nameVal = name.value;
-    // panNumVal = panNum.value;
-    // ageVal = age.value;
-    // qualificationVal = qualification.value;
-
-    let dataPersist = localStorage.getItem("localtask")
-
-    if (dataPersist == null) {
-        dataArr = []
-    } else {
-        dataArr = JSON.parse(dataPersist);
-    }
-    let html = ""
-    //let table = document.getElementById("table");
+function htmlTemplate (dataArr) {
+    let html =""
     let tableDataEl = document.getElementById("main-data");
-    dataArr.forEach((item, index) => {
+
+
+    
+     dataArr.forEach((item, index) => {
         html += `
-        <tr>
+        <tr class="main-row">
           <td>${index + 1}</td>
           <td>${item.personName}</td>
           <td>${item.personPan}</td>
@@ -110,9 +96,64 @@ function showData() {
         
        
        `
+    
     })
+     tableDataEl.innerHTML = html;
+     return html
+}
 
-    tableDataEl.innerHTML = html
+
+
+//Data Submit through modal
+
+submitBtn.addEventListener("click", function (e) {
+    e.preventDefault()
+
+    //getting value from input fields
+    let nameVal = name.value;
+    let panNumVal = panNum.value;
+    let ageVal = age.value;
+    let qualificationVal = qualification.value;
+
+
+    if (nameVal.trim() != "" && panNumVal.trim() != "" && ageVal.trim() != "" && qualificationVal.trim() != "") {
+
+       
+
+        checkAndGetDataPersistAlready()
+
+
+        let obj = {
+            "personName": nameVal,
+            "personPan": panNumVal,
+            "personAge": ageVal,
+            "personQualification": qualificationVal
+        }
+
+        dataArr.push(obj)
+        localStorage.setItem("localData", JSON.stringify(dataArr))
+
+    } else {
+        alert("input fields cannot be empty");
+
+    }
+
+
+    closeModal()
+
+    showData()
+    reset()
+
+})
+
+function showData() {
+  
+
+    checkAndGetDataPersistAlready()
+
+   
+    htmlTemplate(dataArr)
+  
 }
 
 
@@ -120,49 +161,55 @@ function showData() {
 function editData(index) {
 
     saveIdx.value = index
-    let dataPersist = localStorage.getItem("localtask");
-    let dataArr = JSON.parse(dataPersist);
+   
+    checkAndGetDataPersistAlready()
+
     showModal()
-    name.value = dataArr[index].personName
-    panNum.value = dataArr[index].personPan
-    age.value = dataArr[index].personAge
-    qualification.value = dataArr[index].personQualification
+
+    //making the value in inputfield equals of clicked one
+
+    name.value = dataArr[index].personName;
+    panNum.value = dataArr[index].personPan;
+    age.value = dataArr[index].personAge;
+    qualification.value = dataArr[index].personQualification;
+
     submitBtn.style.display = "none";
     saveBtn.style.display = "block";
 
 }
 
 saveBtn.addEventListener("click", function () {
-    let dataPersist = localStorage.getItem("localtask");
-    let dataArr = JSON.parse(dataPersist);
+ 
+    checkAndGetDataPersistAlready();
     let idxValue = saveIdx.value
+
+    //editing data in local storage
+
     dataArr[idxValue].personName = name.value
     dataArr[idxValue].personPan = panNum.value
     dataArr[idxValue].personAge = age.value
     dataArr[idxValue].personQualification = qualification.value
-    localStorage.setItem("localtask", JSON.stringify(dataArr))
+
+    localStorage.setItem("localData", JSON.stringify(dataArr))
     showData()
 })
 
 
 function deleteData(index) {
-    let dataPersist = localStorage.getItem("localtask");
-    let dataArr = JSON.parse(dataPersist);
-    dataArr.splice(index, 1)
-    localStorage.setItem("localtask", JSON.stringify(dataArr))
+  
+    checkAndGetDataPersistAlready();
+    dataArr.splice(index, 1);
+    localStorage.setItem("localData", JSON.stringify(dataArr))
     showData()
 }
 
+
+
 clearAllBtn.addEventListener("click", function () {
-    let dataPersist = localStorage.getItem("localtask");
-    let dataArr = JSON.parse(dataPersist);
-    if (dataPersist == null) {
-        dataArr = []
-    } else {
-        dataArr = JSON.parse(dataPersist);
-        dataArr = []
-    }
-    localStorage.setItem("localtask", JSON.stringify(dataArr))
+  
+    checkAndGetDataPersistAlready()
+    dataArr = [];
+    localStorage.setItem("localData", JSON.stringify(dataArr))
     showData()
 })
 
@@ -170,14 +217,14 @@ clearAllBtn.addEventListener("click", function () {
 const filter = (term) => {
     // console.log(term);
     //console.log(mainDataBox.children)
-    //console.log( Array.from(mainDataBox.children))
+    //console.log( Array.from(mainDataBox.children)) tr
     Array.from(mainDataBox.children).forEach((item) => {
-        //console.log(Array.from(item.children))
-        Array.from(item.children).filter((todo) => {
-            //console.log(todo.textContent)
-            return !todo.textContent.includes(term)
-        }).forEach((todo) => {
-            return todo.parentElement.classList.add("filtered")
+        //console.log(Array.from(item.children))     td
+        Array.from(item.children).filter((data) => {
+            //console.log(data.textContent)
+            return !data.textContent.includes(term)
+        }).forEach((data) => {
+            return data.parentElement.classList.add("filtered")
         })
 
         Array.from(item.children).filter((todo) => {
@@ -196,14 +243,8 @@ searchBox.addEventListener("keyup", () => {
 })
 
 function sortAscending() {
-    console.log(Array.from(mainDataBox.children))
-    let dataPersist = localStorage.getItem("localtask");
-    let dataArr = JSON.parse(dataPersist);
-    // Array.from(mainDataBox.children).forEach((item)=>{
-    //      console.log(Array.from(item.children)[1].innerText.toUpperCase())
-    // })
-    let tableDataEl = document.getElementById("main-data");
-    tableDataEl.innerHTML = ""
+    checkAndGetDataPersistAlready()
+  
     let flag;
     flag = dataArr.sort((a, b) => {
         if (a.personName.toLowerCase() < b.personName.toLowerCase()) {
@@ -214,44 +255,18 @@ function sortAscending() {
         }
         return 0
     })
-    console.log(flag);
-    let html =""
-    flag.forEach((item, index) => {
-        html += `
-        <tr>
-          <td>${index + 1}</td>
-          <td>${item.personName}</td>
-          <td>${item.personPan}</td>
-          <td>${item.personAge}</td>
-          <td>${item.personQualification}</td>
-          <td>
-             <button onclick = "editData(${index})">Edit</button>
-             <button onclick = "deleteData(${index})">Delete</button>
-          
-          </td>
-        
-        
-        </tr>
-       
-        
-       
-       `
-    })
-
-    tableDataEl.innerHTML = html
+    
+    htmlTemplate(flag)
+    localStorage.setItem("localData", JSON.stringify(dataArr))
+    
 
 }
 
 
-function sortDescending(){
-    console.log(Array.from(mainDataBox.children))
-    let dataPersist = localStorage.getItem("localtask");
-    let dataArr = JSON.parse(dataPersist);
-    // Array.from(mainDataBox.children).forEach((item)=>{
-    //      console.log(Array.from(item.children)[1].innerText.toUpperCase())
-    // })
-    let tableDataEl = document.getElementById("main-data");
-    tableDataEl.innerHTML = ""
+function sortDescending() {
+  
+    checkAndGetDataPersistAlready()
+   
     let flag;
     flag = dataArr.sort((a, b) => {
         if (a.personName.toLowerCase() < b.personName.toLowerCase()) {
@@ -262,35 +277,14 @@ function sortDescending(){
         }
         return 0
     })
-    console.log(flag);
-    let html =""
-    flag.forEach((item, index) => {
-        html += `
-        <tr>
-          <td>${index + 1}</td>
-          <td>${item.personName}</td>
-          <td>${item.personPan}</td>
-          <td>${item.personAge}</td>
-          <td>${item.personQualification}</td>
-          <td>
-             <button onclick = "editData(${index})">Edit</button>
-             <button onclick = "deleteData(${index})">Delete</button>
-          
-          </td>
-        
-        
-        </tr>
-       
-        
-       
-       `
-    })
-
-    tableDataEl.innerHTML = html
+ 
+    htmlTemplate(flag)
+    localStorage.setItem("localData", JSON.stringify(dataArr))
+  
 }
 
 sortEl.addEventListener("change", (e) => {
-    console.log(sortEl.value);
+
     if (sortEl.value == "ascending") {
         sortAscending();
     }
